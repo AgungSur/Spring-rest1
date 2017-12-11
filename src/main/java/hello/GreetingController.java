0a38d57e5017c5f67f7aac8947f68b0029cc16cb
ASUS <ASUS@pc>
2017-12-11 18:28:12 +0800

    commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +24,24 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@Autowired
+	private Greeting x;
+	
+	@Autowired
+	private EntityManagerFactory em;
+	
+	@RequestMapping("/actors")
+	public List<Actor> allActor(){
+		return em.createEntityManager()
+				.createQuery("form Actor")
+				.getResultList();
+		
+	}
+	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	 return x;
+		//	return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 	@RequestMapping("/data")
